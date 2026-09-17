29a91a99c66e8cd0b68311e3dddda6e87bd78e49
taoefikarrokhman-ai <taoefik.arrokhman@gmail.com>
2026-09-17 08:23:03 +0700

    Implement basic Blockchain class with methods

---
+++ blockchain.py
@@ -0,0 +1,45 @@
+from block import Block
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = [
+            self.create_genesis_block()
+        ]
+
+    def create_genesis_block(self):
+
+        return Block(
+            index=0,
+            data={
+                "message": "genesis block"
+            },
+            previous_hash="0"
+        )
+    
+    def add_block(self, data: dict):
+
+        previous_block = self.chain[-1]
+
+        new_block = Block(
+            index=len(self.chain),
+            data=data,
+            previous_hash=previous_block.hash
+        )
+
+        self.chain.append(new_block)
+
+    def is_valid(self):
+
+        for i in range(1, len(self.chain)):
+
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
